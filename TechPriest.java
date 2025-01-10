d2064597f257ac2c70835359cbef5571ccf04b4d
Jai Shah <jais5@nycstudents.net>
2025-01-09 22:24:06 -0500

    Created framework for MadAlchemist, fixed techpriest

--- TechPriest.java
+++ TechPriest.java
@@ -2,8 +2,6 @@ public class TechPriest extends Adventurer{
   int faith, faithMax;
   String machine_god;
 
-  /*the other constructors ultimately call the constructor
-  *with all parameters.*/
   public TechPriest(String name, int hp, String machine){
     super(name,hp);
     faithMax = 100;
@@ -23,7 +21,6 @@ public class TechPriest extends Adventurer{
     this("OP-1");
   }
 
-  /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
     return "faith";
   }
@@ -40,22 +37,19 @@ public class TechPriest extends Adventurer{
     return faithMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 caffeine*/
   public String attack(Adventurer other){
 
   }
 
-  /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-  *Reduces caffeine by 8.
-  */
+
   public String specialAttack(Adventurer other){
 
   }
-  /*Restores 5 special to other*/
+
   public String support(Adventurer other){
 
   }
-  /*Restores 6 special and 1 hp to self.*/
+
   public String support(){
 
   }    
