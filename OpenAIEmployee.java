519572370098ede2376e95f77e585a262ad8bea4
William Li <azhlwill@gmail.com>
2025-01-15 09:59:39 -0500

    filled in the information for each move

--- OpenAIEmployee.java
+++ OpenAIEmployee.java
@@ -1,4 +1,4 @@
-public class OpenAIEmployee extends Adventurer{
+  public class OpenAIEmployee extends Adventurer{
   int paycheck, paycheckMax;
 
   /*the other constructors ultimately call the constructor
@@ -6,12 +6,12 @@ public class OpenAIEmployee extends Adventurer{
   public OpenAIEmployee(String name, int hp){
     super(name,hp);
     paycheckMax =100;
-    paycheck = 50;
+    paycheck = 20;
   }
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "paycheck";
+    return "Paycheck";
   }
 
   public int getSpecial(){
@@ -34,19 +34,44 @@ public class OpenAIEmployee extends Adventurer{
     return paycheckMax;
   }
 
+//  deal 15-20 damage to opponent, restores 10 paycheck
   public String attack(Adventurer other){
-
+    int damage = (int)(Math.random()*6) + 15;
+    other.applyDamage(damage);
+    restoreSpecial(10);
+    return this + " throws a stone at " + other + " and dealt " + damage +
+    " points of damage. While attacking, their boss pays them 10 paycheck.";
   }
 
   public String specialAttack(Adventurer other){
-
+    if (getSpecial() >= 40){
+      setSpecial(getSpecial() - 40);
+      int damage = (int)(Math.random() * 50);
+      other.applyDamage(damage);
+      if (damage > 20){
+        return this + " saves up all their paychecks to buy a baseball bat and bashes " +
+        other + " on the head, dealing " + damage + " damage.";
+      }
+      else{
+        return this + " saves up all their paychecks to buy a baseball bat and tries to bash " +
+        other + " on the head, but slightly misses, dealing " + damage + " damage";
+      }
+    }
+    else{
+      return "Not enough paychecks to buy a baseball bat. Instead, " + attack(other);
+    }
   }
 
   public String support(Adventurer other){
-
+    int amount = (int)(Math.random()*6) + 10;
+    other.restoreSpecial(amount);
+    return this + " magically fabricates paychecks and gives them to " + other +
+    " to evade the IRS, restoring " + amount + " paychecks to " + other;
   }
 
   public String support(){
-
+    int amount = (int)(Math.random()*6) + 10;
+    restoreSpecial(amount);
+    return this + " picks up a few pennies, restoring " + amount + " paychecks";
   }
 }
